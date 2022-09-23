a3842ce4dc47a1ebc7c8a43669a5e781bf31dec7
Amanespana <amandeep1552@gmail.com>
2022-09-22 22:34:58 -0400

    added sql script

---
+++ SQLQuery_try1.sql
@@ -0,0 +1,19 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+/*SELECT TOP (1000) [Company]
+      ,[Experience Required]
+      ,[Estimated Salary]
+      ,[Location]
+      ,[Skills]
+  FROM [DSjobs_project].[dbo].[Sheet1$]*/
+
+-- describe function in SQL server
+
+--c sp_help '[dbo].[Sheet1$]'
+
+--cselect count(*) from [DSjobs_project].[dbo].[Sheet1$]
+
+--c select count(distinct Skills) from [DSjobs_project].[dbo].[Sheet1$]
+-- select * from dbo.Sheet1$
+--select count(*) from dbo.Sheet1$ where [Estimated Salary] = 'Not disclosed'
+
+
